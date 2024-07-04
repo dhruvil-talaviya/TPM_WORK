990658f93784a5366fae63ae430e771843ca8124
dhruvil-talaviya <talaviyad380@gmail.com>
2024-07-04 19:47:27 +0530

    many changes

---
+++ server.ts
@@ -0,0 +1,17 @@
+import app from './src/app';
+import { config } from './src/config/config';
+import connectDB from './src/config/db';
+
+const startServer = async () => {
+await connectDB();
+
+
+    const port = config.prot || 4000;
+
+    app.listen(port, () => {
+        console.log(`Listener on port: ${port}`);
+    });
+};
+
+startServer();
+
---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+import {config as conf} from "dotenv";
+
+conf();
+
+const _config = {
+    prot: process.env.PORT,
+    databseURL: process.env.MONGO_CONNECTION_STRING,
+    
+}; 
+
+export const config= Object.freeze(_config);
+
+
+
+
+
